008dc4e16411f5376cda9f38dc0f7e62f3763448
tsf <981879604@qq.com>
2018-05-04 11:36:11 +0800

    tool of word2vec

---
+++ TextSimilarity/data_process/data_config.py
@@ -0,0 +1,26 @@
+
+
+TOKEN_START = "<S>"
+TOKEN_END = "</S>"
+TOKEN_UNKNOWN = "<UNKNOWN>"
+TOKEN_PAD = '<PAD>'
+TOKEN_UNKNOWN_ID = 0
+
+class DataConfig():
+    def __init__(self):
+        self.embedding_dir = ""
+        self.embedding_dim_size = 100
+
+        self.caption_char_txt=""
+        self.char2vec_model = ""
+
+
+        self.caption_word_txt = ""
+        self.word2vec_model = ""
+
+        self.token_unknown=TOKEN_UNKNOWN
+
+        self.seq_max_length = 100
+        self.token_start = TOKEN_START
+        self.token_end = TOKEN_END
+        self.token_pad = TOKEN_PAD
